4c2834b70dbc972f4712d3dac8994636e47250ee
HotMic <116693109+HotMicSystems@users.noreply.github.com>
2024-12-09 21:03:49 -0500

    Update tls-setup.sh
    Added timestamp feature to provide unique certs and prevent overwrites

--- board/raspberrypi/fs_edgemap/opt/edgemap/ca/tls-setup.sh
+++ board/raspberrypi/fs_edgemap/opt/edgemap/ca/tls-setup.sh
@@ -2,9 +2,9 @@
 #
 # This script will create CA and sign a certificate with it.
 #
-# * CA certificate (myCA.crt) is copied to web server document root
-#   so user can download and install it to web browser.
-# * DNS name is equipped as wifi AP name to /etc/hostapd.conf
+# * CA certificate (myCA_<timestamp>.crt) is copied to the web server document root
+#   so the user can download and install it in the web browser.
+# * DNS name is equipped as Wi-Fi AP name to /etc/hostapd.conf
 # * DNS name is equipped to /etc/dnsmasq.conf and /etc/dnsmasq.hosts
 # * DNS name is used as /etc/hostname
 # * DNS name is used at /opt/rnslink/rnslink.ini
@@ -24,50 +24,60 @@ DNS_NAME=$2
 
 if [ -z "$CA_NAME" ]
 then
-echo "Usage: tls-setup.sh [CA-NAME] [DNS-NAME]"
-exit
+  echo "Usage: tls-setup.sh [CA-NAME] [DNS-NAME]"
+  exit
 else
-echo "CA name: $CA_NAME"
+  echo "CA name: $CA_NAME"
 fi
 
 if [ -z "$DNS_NAME" ]
 then
-echo "Usage: tls-setup.sh [CA-NAME] [DNS-NAME]"
-exit
+  echo "Usage: tls-setup.sh [CA-NAME] [DNS-NAME]"
+  exit
 else
-echo "DNS: $DNS_NAME"
+  echo "DNS: $DNS_NAME"
 fi
 
+# Generate a timestamp for unique file names
+TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 
 #
 # Create CA
 #
-openssl req -x509 -new -nodes -newkey rsa:2048 -keyout myCA.key \
-  -sha256 -days 365 -out myCA.crt -subj /CN=$CA_NAME
+CA_KEY="myCA_${TIMESTAMP}.key"
+CA_CERT="myCA_${TIMESTAMP}.crt"
+
+openssl req -x509 -new -nodes -newkey rsa:2048 -keyout "$CA_KEY" \
+  -sha256 -days 365 -out "$CA_CERT" -subj /CN=$CA_NAME
 
 #
 # Create CSR
 #
-openssl req -newkey rsa:2048 -nodes -keyout edgemap.key -out edgemap.csr \
+CSR_KEY="edgemap_${TIMESTAMP}.key"
+CSR_FILE="edgemap_${TIMESTAMP}.csr"
+SIGNED_CERT="edgemap_${TIMESTAMP}.crt"
+
+openssl req -newkey rsa:2048 -nodes -keyout "$CSR_KEY" -out "$CSR_FILE" \
   -subj /CN=edgemap -addext subjectAltName=DNS:$DNS_NAME
+
 #
 # Sign
 #
-openssl x509 -req -in edgemap.csr -copy_extensions copy \
-  -CA myCA.crt -CAkey myCA.key -CAcreateserial -out edgemap.crt -days 365 -sha256
+openssl x509 -req -in "$CSR_FILE" -copy_extensions copy \
+  -CA "$CA_CERT" -CAkey "$CA_KEY" -CAcreateserial -out "$SIGNED_CERT" -days 365 -sha256
 
 #
 # Copy files in place for web server and gwsocket daemons
 #
-cp myCA.crt edgemap.crt edgemap.key /etc/apache2
+cp "$CA_CERT" "$SIGNED_CERT" "$CSR_KEY" /etc/apache2/
 
 #
-# Copy CA for client download via http://edgemap/myCA.crt
+# Copy CA for client download via http://<DNS_NAME>/myCA_<timestamp>.crt
 #
-cp myCA.crt /usr/htdocs/
+cp "$CA_CERT" /usr/htdocs/
 
 #
-# Set wifi AP name
+# Set Wi-Fi AP name
 #
 sed -i "s/^ssid=.*/ssid=${DNS_NAME}/" /etc/hostapd.conf
 
@@ -94,3 +104,4 @@ echo " "
 echo " "
 echo "Done. Reboot unit."
 echo " "
+
